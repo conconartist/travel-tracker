4ba8cc1773b6d4927069ac50c442e8f10567097a
conconartist <conconart.info@gmail.com>
2021-01-16 14:50:10 -0700

    Add travelers data

---
+++ test/data/traveler-data.js
@@ -0,0 +1,13 @@
+let travelers = [
+  { "id": 17,
+    "name": "Julian Ullyott",
+    "travelerType": "relaxer"
+  },
+  {
+    "id": 36,
+    "name": "Lorettalorna Borell",
+    "travelerType": "foodie"
+  }
+]
+
+export default travelers;
