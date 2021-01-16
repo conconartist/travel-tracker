151f8cf49a688f7fdbce7659e082b63bc80b4661
conconartist <conconart.info@gmail.com>
2021-01-15 21:02:51 -0700

    Add test blocks to class Traveler test file

---
+++ test/Traveler-test.js
@@ -0,0 +1,26 @@
+import { expect} from 'chai';
+
+import Traveler from '../src/Traveler.js';
+import traveler1 from '../test/test-data.js';
+import traveler2 from '../test/test-data.js';
+
+let traveler1, traveler2;
+
+describe('Traveler', () => {
+  beforeEach(() => {
+    traveler1 = new Traveler(traveler1);
+    traveler2 = new Traveler(traveler2);
+  });
+  it('should have an id', () => {
+    expect(traveler1.id).to.equal(17);
+    expect(traveler2.id).to.equal(36);
+  });
+  it('should have a name', () => {
+    expect(traveler1.name).to.equal("Julian Ullyott");
+    expect(traveler2.name).to.equal("Lorettalorna Borell");
+  })
+  it('should have a property that suggests their traveler type', () => {
+    expect(traveler1.travelerType).to.equal("relaxer");
+    expect(traveler2.travelerType).to.equal("foodie");
+  })
+})
