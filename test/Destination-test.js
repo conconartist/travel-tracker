bb10e8f718adf3e3c8b6c75e665da5585bf6c557
conconartist <conconart.info@gmail.com>
2021-01-15 21:33:25 -0700

    Add test blocks to class Destination test file

---
+++ test/Destination-test.js
@@ -0,0 +1,31 @@
+import { expect } from 'chai';
+
+import Destination from '../src/Destination.js';
+
+let destination1, destination2, destination3;
+
+describe('Destination', () => {
+    beforeEach(() => {
+        destination1 = new Destination(destinations[0]);
+        destination2 = new Destination(destinations[1]);
+        destination3 = new Destination(destinations[2]);
+    });
+    it('should have an id', () => {
+        expect(destination1.id).to.equal(1);
+    });
+    it('should have a destination property', () => {
+        expect(destination1.destination).to.equal("Lima, Peru");
+    });
+    it('should have an estimated cost of lodging per day', () => {
+        expect(destination2.estimatedLodgingCostPerDay).to.equal(100);
+    });
+    it('should have an estimated cost of flight per person', () => {
+        expect(destination2.estimatedFlightCostPerPerson).to.equal(780);
+    });
+    it('should have a link to an image of the destination', () => {
+        expect(destination2.image).to.equal("https://images.unsplash.com/photo-1560089168-6516081f5bf1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80");
+    });
+    it('should have an alt tag for the image of the destination in a string', () => {
+        expect(destination3.alt).to.equal("opera house and city buildings on the water with boats");
+    });
+});
