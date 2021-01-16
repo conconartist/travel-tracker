706809c3519bb6d3fa546cb15b00c0416977d0ff
conconartist <conconart.info@gmail.com>
2021-01-16 14:49:24 -0700

    Update Traveler test file

--- test/Traveler-test.js
+++ test/Traveler-test.js
@@ -1,7 +1,7 @@
 import { expect} from 'chai';
 
 import Traveler from '../src/Traveler.js';
-import travelers from '../test/test-data.js';
+import travelers from '../test/data/traveler-data.js';
 
 let traveler1, traveler2;
 
@@ -10,14 +10,18 @@ describe('Traveler', () => {
     traveler1 = new Traveler(travelers[0]);
     traveler2 = new Traveler(travelers[1]);
   });
+
   it('should have an id', () => {
+    console.log(traveler1)
     expect(traveler1.id).to.equal(17);
     expect(traveler2.id).to.equal(36);
   });
+
   it('should have a name', () => {
     expect(traveler1.name).to.equal("Julian Ullyott");
     expect(traveler2.name).to.equal("Lorettalorna Borell");
   });
+  
   it('should have a property that suggests their traveler type', () => {
     expect(traveler1.travelerType).to.equal("relaxer");
     expect(traveler2.travelerType).to.equal("foodie");
