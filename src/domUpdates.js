c4db0f15678172b20ac8c94db6dfa2f7a722a08c
conconartist <conconart.info@gmail.com>
2021-01-17 20:59:11 -0700

    Add to domUpdates with display functionality

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -9,13 +9,28 @@ let domUpdates = {
         //toggle to expand and hide 
     },
 
-    displayUpcomingTrips() {
-
+    displayBookTrip() {
+        let bookTripSection = document.querySelector('.card-book-trip');
+        //remove hidden class 
+    },
+    
+    displayUpcomingTrips(dateToday) {
+        let upcomingTripsSection = document.querySelector('.card-upcoming-trips');
+        upcomingTripsSection = '';
+        let upcomingTripsList = traveler.filterTrips(tripData).filter(trip => trip.date >= dateToday);
+        upcomingTripsList.forEach(trip => {
+            upcomingTripsSection.insertAdjacentHTML('afterbegin', `
+            <p class="display-date upcoming-date">Date: ${trip.date}</p>
+            <p class="display-destination upcoming-destination">Destination: </p>
+            <p class="display-duration upcoming-duration">Duration: ${trip.duration}</p>
+            <p class="display-number-travelers upcoming-travelers"># of Travelers: ${trip.travelers}</p>
+            `)
+        })
     },
 
     displayPendingTrips(traveler) {
         let pendingTripsSection = document.querySelector('.card-pending-trips');
-        pendingTrips.innerHTML = '';
+        pendingTripsSection.innerHTML = '';
         let pendingTripsList = traveler.filterTrips(tripData).filter(trip => trip.status === 'pending');
         pendingTripsList.forEach(trip => {
             pendingTripsSection.insertAdjacentHTML('afterbegin', `
@@ -27,12 +42,19 @@ let domUpdates = {
         })
     },
 
-    displayBookTrip() {
-
-    },
-
-    displayPastTrips() {
-
+    displayPastTrips(traveler, dateToday) {
+        let pastTripsSection = document.querySelector('.card-past-trip');
+        pastTripsSection.innerHTML = '';
+        let pastTrips = traveler.filterTrips(tripData).filter(trip => trip.date <= dateToday) 
+        )
+        pastTrips.forEach(trip => {
+            pastTripsSection.insertAdjacentHTML('afterbegin', `
+            <p class="display-date">Date: ${trip.date}</p>
+            <p class="display-destination">Destination:</p>
+            <p class="display-duration">Duration: ${trip.duration}</p>
+            <p class="display-number-travelers"># of Travelers: ${trip.travelers}</p>
+            `)
+        })
     },
 
     displayTotalAnnualAmt(traveler, tripData, year, destinations) {
